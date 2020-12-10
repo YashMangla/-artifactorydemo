bcfb790e67716d167250dfac762c2c9ffa5e1e70
YashMangla <ymangla37@gmail.com>
2020-12-10 12:28:52 +0530

    updated test1 file

--- updatescript.sh
+++ updatescript.sh
@@ -1,8 +1,21 @@
+rm -rf test.txt
+echo "this is a test2 file" >> test1.txt
+git add .
+git commit -m "updated test1 file"
+git push - origin main
 git pull
 hash=$(git log --pretty=format:'%h' -n 1)
 echo $hash
 folderstr=$(git diff-tree --no-commit-id --name-only -r $hash) # > /Users/diptripa/git-updated-files/difference.txt
 #cat /Users/diptripa/git-updated-files/difference.txt
 echo $folderstr
+git status
 
-curl -u admin:admin -T $folderstr "http://localhost/artifactory/artidemo/$folderstr"
+for i in $folderstr
+do 
+	# get all the list of changed files
+	#echo $i 
+	current_path=$(pwd)"/"$i
+	#echo $current_path
+	#curl -u admin:admin -T $current_path "http://localhost/artifactory/artidemo/$i"
+done
