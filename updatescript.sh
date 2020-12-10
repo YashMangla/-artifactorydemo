22a43be90260b68ee1a8ebd5fb4812f51a84c88a
YashMangla <ymangla37@gmail.com>
2020-12-10 12:34:04 +0530

    updated test file

--- updatescript.sh
+++ updatescript.sh
@@ -1,7 +1,7 @@
 rm -rf test2.txt
 #echo "this is a test2 file" >> test2.txt
 git add .
-git commit -m "updated test2 file"
+git commit -m "updated test file"
 git push -origin main
 git status
 git pull
